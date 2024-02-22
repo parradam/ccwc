1d7df0f0edf92e848ad1790714549eba8d3922c7
Adam Parr <adamparr.uk@gmail.com>
2024-02-22 21:50:17 +0000

    add character count functionality

--- main.py
+++ main.py
@@ -14,18 +14,26 @@ def print_byte_count(filename):
 
 
 def print_line_count(filename):
-    with open(filename, "r") as f:
+    with open(filename, "rb") as f:
         line_count = sum(1 for _ in f)
     print(line_count, filename)
 
 
 def print_word_count(filename):
     word_count = 0
-    with open(filename, "r") as f:
+    with open(filename, "rb") as f:
         for line in f:
             words = line.split()
             word_count += len(words)
     print(word_count, filename)
+
+
+def print_character_count(filename):
+    character_count = 0
+    with open(filename, "rb") as f:
+        for line in f:
+            character_count += len(line.decode())
+    print(character_count, filename)
 
 
 if __name__ == "__main__":
@@ -37,6 +45,9 @@ if __name__ == "__main__":
     parser.add_argument("-c", help="count the number of bytes", action="store_true")
     parser.add_argument("-l", help="count the number of lines", action="store_true")
     parser.add_argument("-w", help="count the number of words", action="store_true")
+    parser.add_argument(
+        "-m", help="count the number of characters", action="store_true"
+    )
     parser.add_argument("filename", help="the filename to be checked")
     args = parser.parse_args()
 
@@ -49,3 +60,5 @@ if __name__ == "__main__":
         print_line_count(args.filename)
     if args.w:
         print_word_count(args.filename)
+    if args.m:
+        print_character_count(args.filename)
