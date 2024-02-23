fd696961ee2df7f0d13046ff14364c7ca89cde05
Adam Parr <adamparr.uk@gmail.com>
2024-02-23 21:12:26 +0000

    reverse boolean, add extra arguments

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 
 
 def file_exists(filename):
-    if not os.path.exists(filename):
-        return False
-    return True
+    if os.path.exists(filename):
+        return True
+    return False
 
 
 def get_byte_count(filename):
@@ -48,25 +48,35 @@ if __name__ == "__main__":
         description="Finds the number of bytes, words, characters, and multibyte characters in a text file",
     )
 
-    parser.add_argument("-c", help="count the number of bytes", action="store_true")
-    parser.add_argument("-l", help="count the number of lines", action="store_true")
-    parser.add_argument("-w", help="count the number of words", action="store_true")
     parser.add_argument(
-        "-m", help="count the number of characters", action="store_true"
+        "-c", "--byte", help="count the number of bytes", action="store_true"
+    )
+    parser.add_argument(
+        "-l", "--line", help="count the number of lines", action="store_true"
+    )
+    parser.add_argument(
+        "-w", "--word", help="count the number of words", action="store_true"
+    )
+    parser.add_argument(
+        "-m", "--char", help="count the number of characters", action="store_true"
     )
     parser.add_argument("filename", help="the filename to be checked")
     args = parser.parse_args()
 
-    if not file_exists(args.filename):
+    if args.filename and not file_exists(args.filename):
         raise argparse.ArgumentTypeError(f"The file {args.filename} does not exist.")
 
-    if args.c:
+    if args.filename:
+        with open(args.filename, "rb") as f:
+            data = f.read()
+
+    if args.char:
         results = [get_byte_count(args.filename)]
-    elif args.l:
+    elif args.line:
         results = [get_line_count(args.filename)]
-    elif args.w:
+    elif args.word:
         results = [get_word_count(args.filename)]
-    elif args.m:
+    elif args.char:
         results = [get_character_count(args.filename)]
     else:
         results = [
